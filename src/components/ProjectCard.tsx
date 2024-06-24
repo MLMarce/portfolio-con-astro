550c35e9f6c1897816b3c170932603ca1ebe87ec
Marcelo Lencina <marcelodaniellencina@gmail.com>
2024-06-23 22:03:32 -0300

    fix project close btn

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -11,13 +11,13 @@ interface Props {
 }
 export default function ProjectCard({ title, description, technologies, date, setFullProject }: Props) {
   return (
-    <article className="absolute z-[100] w-full min-h-full h-max p-4 flex flex-col justify-between items-center bg-gradient-to-b rounded-lg top-0 right-0 left-0 bottom-0">
+    <article className="absolute z-[100] w-full min-h-full h-max p-4 flex flex-col justify-between items-center bg-gradient-to-b rounded-lg top-0 right-0 left-0 bottom-0 overflow-x-hidden">
       <span
         onClick={() => setFullProject(null)}
-        className="text-4xl cursor-pointer w-12 h-12 text-center flex justify-center items-center absolute -top-10 right-4 p-2 bg-white text-black rounded-full"
+        className="text-xl font-bold cursor-pointer w-10 h-8 text-center flex justify-center items-center absolute -top-[9px] -right-2 pt-1 pr-1 bg-white text-black rounded-full"
       >X</span>
       <header className="w-full px-2 flex flex-col justify-between">
-        <h2 className="text-md">{title}</h2>
+        <h2 className="text-md font-bold">{title}</h2>
         <p className="text-sm">{date}</p>
       </header>
       <p className="text-[16px] font-normal">{description}</p>
